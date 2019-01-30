2efe097bdbbd897cbbad7351b0fd97aa182b749c
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-01-30 17:42:56 +0400

    Add courses image

---
+++ courses/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker build -t kuberlab/fastai:0.7.0 -f $SCRIPT_DIR/Dockerfile .
+docker build -t kuberlab/fastai:0.7.0-gpu -f $SCRIPT_DIR/Dockerfile.gpu .
+
+docker push kuberlab/fastai:0.7.0
+docker push kuberlab/fastai:0.7.0-gpu
