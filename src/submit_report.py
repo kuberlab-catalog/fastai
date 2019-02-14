76e4207543caf871e33f2061c2d878020bd2bd2b
Vadim <monstarnn@gmail.com>
2019-02-14 18:05:31 +0300

    lesson in job

--- src/submit_report.py
+++ src/submit_report.py
@@ -1,6 +1,8 @@
 from mlboardclient.api import client
 import sys
 
-with open(sys.argv[1], 'r') as in_file:
-    data=in_file.read()
-    client.update_task_info({'#documents.report.html':data})
+data = ""
+for line in sys.stdin:
+    data += line
+
+client.update_task_info({'#documents.report.html':data})
